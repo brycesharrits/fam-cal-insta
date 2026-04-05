f1ed157d2d26ce2a0135f3165f5c61b4c876465c
Bryce Sharrits <brycesharrits@gmail.com>
2026-04-05 11:52:48 -0400

    Initial Commit

---
+++ FamCalInsta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FamCalInsta
+//
+//  Created by Bryce Sharrits on 4/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FamCalInsta/FamCalInstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamCalInstaApp.swift
+//  FamCalInsta
+//
+//  Created by Bryce Sharrits on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FamCalInstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
